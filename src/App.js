a7768e194d1a1cb622621d2357a82a2dd31b8be7
xxxmarxxx <50579679+xxxmarxxx@users.noreply.github.com>
2021-02-09 11:33:13 +0100

    lesson 188 add main and section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,25 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
+// lesson 188/
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "pagintion"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {data.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+      </section>
+    </main>
+  );
 }
 
-export default App
+export default App;
